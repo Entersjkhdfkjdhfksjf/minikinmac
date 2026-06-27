7aec345e4fe75a2681f039114c93bc763d6a9bac
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2026-06-27 21:41:05 +0530

    Refactor launch.sh by removing comments
    Removed comments and simplified the launch script.

--- kual_ext/launch.sh
+++ kual_ext/launch.sh
@@ -2,18 +2,15 @@
 
 cd "$(dirname "$0")"
 
-# 1. Gracefully stop Kindle UI
 stop lab126_gui
 eips -c
 eips -c
 
-# 2. Launch the statically linked emulator
-# (Any terminal output/errors will be saved here so we aren't blind)
 ./bin/minivmac > bash_crash.log 2>&1
-
-# 3. Sync to flash and restore
 sync
+
 eips -c
 start lab126_gui
+
 
 
